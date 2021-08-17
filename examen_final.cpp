f7e763ad2c236347e4e742f2a29231cba83ccd28
Je12emy <Jeremyzelaya@hotmail.es>
2021-08-16 20:42:28 -0600

    Examen de Julian

--- examen_final.cpp
+++ examen_final.cpp
@@ -21,7 +21,6 @@ int main(){
 
         cout << "1. Contador de letras." << endl;
         cout << "2. Imprimir una figura." << endl;
-        cout << "3. Calculador de valores." << endl;
 
         cout << "Seleccione un programa: " << "";
         cin >> opcion;
@@ -92,90 +91,6 @@ int main(){
                     }
 
 
-                }
-                break;
-            case 3:
-                {
-                    const int cantidad_numeros = 3;
-                    bool continuar = true;
-                    int valor;
-                
-                    int numero1;
-                    int numero2;
-                    int numero3;
-
-                    // Capturar valores
-                    for(int i = 0; i < cantidad_numeros; i++) {
-                        cout << "Ingrese un numero para el valor " << i+1 << ": ";
-                        cin >> valor;
-                        
-                        // Verificar si el valor tiene dos digitos
-                        if (valor < 10 || valor >= 100) {
-                            cout << "Ese no es un valor valido!" << endl;
-                            continuar = false;
-                            break;
-                        } else {
-                            // Asignar los valores de cada variable
-                            switch(i) {
-                                case 0:
-                                    numero1 = valor;
-                                break;
-                                case 1:
-                                    numero2 = valor;
-                                break;
-                                case 2:
-                                    numero3 = valor;
-                                break;
-                            }
-                        }
-                    }
-                    
-
-                    // Evaluar si hay un valor repetido
-                    if(numero1 == numero2 || numero1 == numero3) {
-                        cout << "No se repiten numero!" << endl
-                        continuar = false;
-                    }
-                    if(numero3 == numero2 || numero3 == numero1) {
-                        cout << "No se repiten numero!" << endl
-                        continuar = false;
-                    }
-
-                    if(continuar) {
-                        // Calcular resultados
-                        int suma = numero1 + numero2 + numero3;
-                        float promedio = suma / 3;
-                        int producto = numero1 * numero2 * numero3;
-
-                        int menor;
-                        int mayor;
-
-                        cout << "- Suma de los numeros: " << suma << endl;
-                        cout << "- Promedio de los numeros:" << promedio << endl;
-                        cout << "- Producto de los numeros:" << producto << endl;
-
-                        // Obtener al menor
-                        if(numero1 < numero2 && numero1 < numero3) {
-                            menor = numero1;
-                        } else if (numero2 < numero1 && numero2 < numero3 ) {
-                            menor = numero2;
-                        } else {
-                            menor = numero3;
-                        }
-
-                        cout << "- Valor menor:" << menor << endl;
-
-                        // Obtener al mayor
-                        if(numero1 > numero2 && numero1 > numero3) {
-                            mayor = numero1;
-                        } else if (numero2 > numero1 && numero2 > numero3 ) {
-                            mayor = numero2;
-                        } else {
-                            mayor = numero3;
-                        }
-
-                        cout << "- Valor mayor: " << mayor << endl;
-                    }
                 }
                 break;
             default:
